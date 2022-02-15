03797bcc459912f02d8d9786ab36c88e999493d3
edwin <edwinslottje@gmail.com>
2022-02-15 15:28:18 +0100

    Push for some small update (no big changes)

--- ProjectInovationCard/Assets/Scripts/NFC_ActiveReading.cs
+++ ProjectInovationCard/Assets/Scripts/NFC_ActiveReading.cs
@@ -10,7 +10,7 @@ public class NFC_ActiveReading : MonoBehaviour
     void Start()
     {
         NFC.Instance.EnableReaderMode();
-
+        SetupNFCScanner();
     }
 
     void Update()
@@ -23,8 +23,20 @@ public class NFC_ActiveReading : MonoBehaviour
         // ndef tag detected
         if (NFC.isSupported)
         {
-            Debug.Log("Working on android!!");
-            displayText.text = "it works";
+            //Debug.Log("Working on android!!");
+            displayText.text = "Nfc tag scanned now processing data to see what to do...";
+            //displayText.text = e.tag.messages[0].records[0].id;
+            //for (int i = 0; i < e.tag.messages[i].records.Count; i++)
+            //{
+            //    displayText.text = e.tag.messages[0].records[0].payload;
+            //}
         }
+    }
+
+    void SetupNFCScanner()
+    {
+        //ScanOptions options = new ScanOptions();
+        //options.urls = new string[] { "This is a power card" };
+        //NFC.Instance.RegisterForegroundDispatch(options);
     }
 }
