4f21a21f059b61ba723471595788ef8d3fb4b81e
edwin <edwinslottje@gmail.com>
2022-02-17 12:44:17 +0100

    Code refactored - Nfc working for powercard 1-4

--- ProjectInovationCard/Assets/Scripts/NFC_Reader.cs
+++ ProjectInovationCard/Assets/Scripts/NFC_Reader.cs
@@ -30,35 +30,58 @@ public class NFC_Reader : MonoBehaviour
         {
             if (!tagFound)
             {
-                try
-                {
-                    // Create new NFC Android object
-                    mActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"); // Activities open apps
-                    mIntent = mActivity.Call<AndroidJavaObject>("getIntent");
-                    sAction = mIntent.Call<String>("getAction"); // resulte are returned in the Intent object
-                    if (sAction == "android.nfc.action.NDEF_DISCOVERED")
-                    {
-                        Debug.Log("Tag of type NDEF");
-                        AndroidJavaObject[] rawMsg = mIntent.Call<AndroidJavaObject[]>("getParcelableArrayExtra", "android.nfc.extra.NDEF_MESSAGES");
-                        AndroidJavaObject[] records = rawMsg[0].Call<AndroidJavaObject[]>("getRecords");
-                        byte[] payLoad = records[0].Call<byte[]>("getPayload");
-                        string result = System.Text.Encoding.Default.GetString(payLoad); // not sure if it works for all encodings, but it works for me
-
-                        tag_output_text.text = result;
-                        if (result == "PowerCard.1")
-                        {
-                            
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    string text = ex.Message;
-                    tag_output_text.text = text;
-                }
+                PlaySounds();
+                
             }
         }
     }
 
-    
+    string ScanNFC()
+    {
+        try
+        {
+            // Create new NFC Android object
+            mActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"); // Activities open apps
+            mIntent = mActivity.Call<AndroidJavaObject>("getIntent");
+            sAction = mIntent.Call<String>("getAction"); // resulte are returned in the Intent object
+            if (sAction == "android.nfc.action.NDEF_DISCOVERED")
+            {
+                Debug.Log("Tag of type NDEF");
+                AndroidJavaObject[] rawMsg = mIntent.Call<AndroidJavaObject[]>("getParcelableArrayExtra", "android.nfc.extra.NDEF_MESSAGES");
+                AndroidJavaObject[] records = rawMsg[0].Call<AndroidJavaObject[]>("getRecords");
+                byte[] payLoad = records[0].Call<byte[]>("getPayload");
+                string result = System.Text.Encoding.Default.GetString(payLoad); // not sure if it works for all encodings, but it works for me
+
+                tag_output_text.text = result;
+                return result;
+            }
+        }
+        catch (Exception ex)
+        {
+            string text = ex.Message;
+            tag_output_text.text = text;
+            return text;
+        }
+        return "no scan";
+    }
+
+    void PlaySounds()
+    {
+        if(ScanNFC() == "PowerCard.1")
+        {
+
+        }
+        if (ScanNFC() == "PowerCard.2")
+        {
+
+        }
+        if (ScanNFC() == "PowerCard.3")
+        {
+
+        }
+        if (ScanNFC() == "PowerCard.4")
+        {
+
+        }
+    }
 }
