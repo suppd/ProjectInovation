3df40293bac1acf6faf56123290328c2696c46f2
edwin <Edwinslottje@gmail.com>
2022-03-02 01:42:54 +0100

    Randomized sounds added

--- ProjectInovationCard/Assets/Scripts/CardManager.cs
+++ ProjectInovationCard/Assets/Scripts/CardManager.cs
@@ -40,10 +40,13 @@ public class CardSounds
     public AudioSource strongCard;
 
     public string SoundName;
+
     public GameObject pointSound;
+    public GameObject specialSound;
     void Start()
     {
         pointCardSounds.Add(pointSound.GetComponent<AudioSource>());
+        specialCardSounds.Add(pointSound.GetComponent<AudioSource>());
     }
     public enum CardType
     {
@@ -52,7 +55,28 @@ public class CardSounds
         shifter
     };
 
-    
+    void RandomizePointSounds()
+    {
+        for (int i = 0; i < pointCardSounds.Count; i++)
+        {
+            AudioSource temp = pointCardSounds[i];
+            int randomIndex = Random.Range(i, pointCardSounds.Count);
+            pointCardSounds[i] = pointCardSounds[randomIndex];
+            pointCardSounds[randomIndex] = temp;
+        }
+    }
+
+    void RandomizeSpecialCards()
+    {
+        for (int i = 0; i < specialCardSounds.Count; i++)
+        {
+            AudioSource temp = specialCardSounds[i];
+            int randomIndex = Random.Range(i, specialCardSounds.Count);
+            specialCardSounds[i] = specialCardSounds[randomIndex];
+            specialCardSounds[randomIndex] = temp;
+        }
+    }
+
 }
 
 public class CardManager : MonoBehaviour
