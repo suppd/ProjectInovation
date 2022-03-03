7f568a9f9d6efba505b766212cddcdf13402a334
edwin <edwinslottje@gmail.com>
2022-03-03 17:23:20 +0100

    sound system push 3

--- ProjectInovationCard/Assets/SoundPlayer.cs
+++ ProjectInovationCard/Assets/SoundPlayer.cs
@@ -4,36 +4,47 @@ using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour
 {
-    public AudioClip[] sounds;
+    public AudioClip[] pointSounds;
+    public AudioClip[] specialSounds;
     public float[] volumes;
 
     AudioSource audioSource;
+    //public AudioSource audioSourceSpecial;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        
         // TODO: verify that both arrays have the same length!
     }
 
     public void PlaySound(int index)
     {
-        if (index>=0 && index<sounds.Length)
+        if (index>=0 && index<pointSounds.Length  && !audioSource.isPlaying)
         {
-            audioSource.PlayOneShot(sounds[index],volumes[index]); // may give out of range error!
+            audioSource.PlayOneShot(pointSounds[index],volumes[index]); // may give out of range error!
+        }
+    }
+
+    public void PlaySpecialSound(int index)
+    {
+        if (index >= 0 && index < pointSounds.Length && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(pointSounds[index], volumes[index]); // may give out of range error!
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // just for debugging:
-        for (int i=0;i<sounds.Length;i++)
-        {
-            if (Input.GetKeyDown((KeyCode)(i+48))) // hacky!
-            {
-                PlaySound(i);
-            }
-        }
+        //// just for debugging:
+        //for (int i=0;i<sounds.Length;i++)
+        //{
+        //    if (Input.GetKeyDown((KeyCode)(i+48))) // hacky!
+        //    {
+        //        PlaySound(i);
+        //    }
+        //}
     }
 }
